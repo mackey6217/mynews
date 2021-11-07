6d8c60f830a04e6efb113329abe7ef3b2b549cdd
shoya <m.syoya0508@gmail.com>
2021-11-07 13:16:33 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name "viewport" content="width=device-width, initial-scale=1">
+        
+        <title>PHP10kadai</title>
+        
+        <style>
+            h1 {
+                color: green;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>プロフィール作成画面</h1>
+    </body>
+</html>
+
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
