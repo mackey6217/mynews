7e4f78952884befe572c6fd072a595d2c54bc814
shoya <m.syoya0508@gmail.com>
2021-11-27 14:41:29 +0000

    commit12

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,16 +8,20 @@
                     <div class="post">
                         <div class="row">
                             <div class="text col-md-6">
+                                <h4>（名前）</h4>
                                 <div class="name">
                                     {{ str_limit($post->name, 20) }}
                                 </div>
                                 <div class="gender mt-3">
+                                    <h4>（性別）</h4>
                                     {{ str_limit($post->gender, 10) }}
                                 </div>
                                 <div class="hobby">
+                                    <h4>（趣味）</h4>
                                     {{ str_limit($post->hobby, 100) }}
                                 </div>
                                 <div class="introduction">
+                                    <h4>（自己紹介）</h4>
                                     {{ str_limit($post->introduction, 300) }}
                                 </div>
                             </div>
