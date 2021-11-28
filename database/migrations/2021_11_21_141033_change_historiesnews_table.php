93e5f02548ecf027142e67ff0240e563f14d3483
shoya <m.syoya0508@gmail.com>
2021-11-28 05:56:11 +0000

    commit14

--- database/migrations/2021_11_21_141033_change_historiesnews_table.php
+++ database/migrations/2021_11_21_141033_change_historiesnews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeHistoriesnewsTable extends Migration
+class Change_Histories_news_Table extends Migration
 {
     /**
      * Run the migrations.
